71d3e6fcc7a9a49bcf48bf6e5eda8ec405322653
Bishal <bishals10@gmail.com>
2024-02-24 01:49:34 +0200

    add functionality to the game using React Ref

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,19 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+  const player = useRef();
+
+  function handleClick() {
+    setEnteredPlayerName(player.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={player} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
